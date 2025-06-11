1119d2f526f6c7b8102e5851f417f2305366ea33
Kick_D <zeratain@gmail.com>
2025-06-12 00:50:42 +0300

    Rock / Paper / Scissors v1.0

--- Rock_Paper_scissors.py
+++ Rock_Paper_scissors.py
@@ -1,12 +1,52 @@
-import math
 import random
 
-from accessify import protected, private
+user_wins = 0
+computer_wins = 0
 
-# test 1 fr Pet-prject in GitHub
-# finish test for work with GitHub into Code_Runer_Repo
+options = ["ROCK", "PAPER", "SCISSORS"]
 
-# I am working with Code_Runer_Repo_FORK now!!!
-# Already merge fork version with origin version 
+while True:
+    user_input = " ".join(input('Type Rock/Paper/Scissors or Q to quit. ').upper().split())
+    if user_input == "Q":
+        break
+    
+    if user_input not in options:
+        print('Value is not valid, try again')
+        continue
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print(f'Computer picked: {computer_pick}')
+
+    if user_input == 'ROCK' and computer_pick == 'SCISSORS':
+        print('You won!')
+        user_wins += 1
+
+    elif user_input == 'PAPER' and computer_pick == 'ROCK':
+        print('You won!')
+        user_wins += 1
+    
+    elif user_input == 'SCISSORS' and computer_pick == 'PAPER':
+        print('You won!')
+        user_wins += 1
+    
+    elif user_input == computer_pick:
+        print('its a Tie!')
+    
+    else:
+        print('You lost!')
+        computer_wins += 1
+
+print(f'You won {user_wins} times')
+print(f'Computer wins {computer_wins} times')
+print('Bye)')
+    
+
+
+
+
+
+    
 
 
