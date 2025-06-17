63285d0d6c24c180c552e75313aae4b31f7335e6
Kick_D <zeratain@gmail.com>
2025-06-17 12:38:34 +0300

    Start right branch history

--- Pick_your_adventure_game.py
+++ Pick_your_adventure_game.py
@@ -4,9 +4,16 @@ print(f'Welcome {name} to this adventure!')
 answer = input("Which way would you like to go? <--(left) or (right)-->  ").lower()
 
 if answer == 'left':
-    answ2 = input('You come to a river, you can walk around it or swim accross... <--(swim) or (walk)-->').lower()
+    answer = input('You come to a river, you can walk around it or swim accross... <--(swim) or (walk)-->').lower()
+    if answer == "swim":
+        print("You swam acrross and were eaten by alligator.")
+    elif answer == "walk":
+        print('You walked for many miles, run out of water. You lose the game...')
+    else:
+        print('Not a valid option! You lose')
 
 elif answer =='right':
-    print('')
+    print('Start')
+
 else:
     print('Not a valid option! You lose')
