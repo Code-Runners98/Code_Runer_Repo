1f731f19ec833fa808d07a74f2807c208bf9b63c
Kick_D <zeratain@gmail.com>
2025-06-17 12:15:26 +0300

    histories ab river version 0.01

--- Pick_your_adventure_game.py
+++ Pick_your_adventure_game.py
@@ -1,3 +1,12 @@
 name = input('Type your name:')
 print(f'Welcome {name} to this adventure!')
 
+answer = input("Which way would you like to go? <--(left) or (right)-->  ").lower()
+
+if answer == 'left':
+    answ2 = input('You come to a river, you can walk around it or swim accross... <--(swim) or (walk)-->').lower()
+
+elif answer =='right':
+    print('')
+else:
+    print('Not a valid option! You lose')
