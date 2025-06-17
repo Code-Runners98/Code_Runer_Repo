7e76e696619073d1fc9437d7c89b50379f175d86
Kick_D <zeratain@gmail.com>
2025-06-17 13:34:33 +0300

    right/back branch history added

--- Pick_your_adventure_game.py
+++ Pick_your_adventure_game.py
@@ -13,7 +13,20 @@ if answer == 'left':
         print('Not a valid option! You lose')
 
 elif answer =='right':
-    print('Start')
+    answer = input('You come to a bridge, it looks wobbly, do you want to cross it or head back? <--(cross) (back)--> ')
+    if answer == "back":
+        print("You go back to the main road. An old man, dying under a stone standing at a crossroads, calls you to come to him.")
+        answer = input('You can stab an old man with his own pitchfork or approach him <--(stab) (approach)-->')
+        if answer == "stab":
+            print("You pierce his stomach and realize that there is emptiness inside, the old man bursts into flames and blows himself up. You died")
+        elif answer == "approach":
+            print('You approach him, tilt your head to hear what he wants to say. He says you won.')
+        else:
+            print('Not a valid option! You lose') 
+    elif answer == "cross":
+        print('')
+    else:
+        print('Not a valid option! You lose')
 
 else:
     print('Not a valid option! You lose')
