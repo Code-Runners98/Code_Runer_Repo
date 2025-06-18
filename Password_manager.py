1730534d3eacf03e20b73d4fd44a8b291242b8ec
Kick_D <zeratain@gmail.com>
2025-06-18 23:44:36 +0300

    def view/def create mostly worked, password.txt added

--- Password_manager.py
+++ Password_manager.py
@@ -1,25 +1,32 @@
-import random
-from accessify import private, protected
-
-pwd = input('What is the master password? ').lower()
+master_pwd = input('What is the master password? ').lower()
 
 mode = int(input('1.Create a new password / 2.View existing ones / 3.To quit '))
 
 def view():
-    pass
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split(' | ')
+            print(f'User: {user}, Password: {passw}')
+
 
 def create():
-    pass
+    name = input('Account name: ')
+    pwd = input('Password: ') 
+
+    with open('passwords.txt', 'a') as f:
+        f.write(f'{name} | {pwd}' + '\n')
+
 
 while True:
     if mode == 3:
         print('Quit from program')
         quit()
     if mode == 2:
-        print('Work')
-        pass
+        view()
+        break
     elif mode == 1:
-        pass
+        create()
     else:
         print('Incorrect value for mode, use numbers to pick the mode')
         continue
