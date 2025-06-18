2371a9c57769fe0000d55390b5eb0bdb6fb0a52c
Kick_D <zeratain@gmail.com>
2025-06-18 22:52:44 +0300

    Checking significantly, def view / def create added

--- Password_manager.py
+++ Password_manager.py
@@ -3,4 +3,23 @@ from accessify import private, protected
 
 pwd = input('What is the master password? ').lower()
 
-mode = input('1.Create a new password / 2.View existing ones ').lower()
+mode = int(input('1.Create a new password / 2.View existing ones / 3.To quit '))
+
+def view():
+    pass
+
+def create():
+    pass
+
+while True:
+    if mode == 3:
+        print('Quit from program')
+        quit()
+    if mode == 2:
+        print('Work')
+        pass
+    elif mode == 1:
+        pass
+    else:
+        print('Incorrect value for mode, use numbers to pick the mode')
+        continue
