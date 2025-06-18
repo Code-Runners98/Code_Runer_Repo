3e4fa98d3560e7756c20c9b57a50926cc6c24c51
Kick_D <zeratain@gmail.com>
2025-06-18 22:25:46 +0300

    master password/mode input added

---
+++ Password_manager.py
@@ -0,0 +1,6 @@
+import random
+from accessify import private, protected
+
+pwd = input('What is the master password? ').lower()
+
+mode = input('1.Create a new password / 2.View existing ones ').lower()
