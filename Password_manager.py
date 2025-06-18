6e95dc8d9d70e89b5d2c681fe464b0aa55441b54
Kick_D <zeratain@gmail.com>
2025-06-19 00:25:08 +0300

    Pass manager v0.01

--- Password_manager.py
+++ Password_manager.py
@@ -1,13 +1,27 @@
-master_pwd = input('What is the master password? ').lower()
+from cryptography.fernet import Fernet
 
-mode = int(input('1.Create a new password / 2.View existing ones / 3.To quit '))
+"""
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)"""
+
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
+master_pwd = input('What is the master password? ').lower()
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split(' | ')
-            print(f'User: {user}, Password: {passw}')
+            print(f'User: {user}, Password: {fer.decrypt(passw.encode()).decode()}')
 
 
 def create():
@@ -15,10 +29,12 @@ def create():
     pwd = input('Password: ') 
 
     with open('passwords.txt', 'a') as f:
-        f.write(f'{name} | {pwd}' + '\n')
+        f.write(f'{name} | {fer.encrypt(pwd.encode()).decode()}' + '\n')
 
 
 while True:
+    mode = int(input('1.Create a new password / 2.View existing ones / 3.To quit '))
+
     if mode == 3:
         print('Quit from program')
         quit()
