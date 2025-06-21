45dfeda71e71a7c9899317b534e20e1a6859d6bd
Kick_D <zeratain@gmail.com>
2025-06-21 22:34:30 +0300

    finish PIG prj

--- PIG.py
+++ PIG.py
@@ -14,11 +14,43 @@ while True:
 
         if 2 <= players <= 4:
             break
-        
+
         else:
             print('Value of players must be from 2 to 4. Try again')
 
     else:
         print('You must use numbers(from 2 to 4) to chouse numbers of players')
 
-print(players)
+max_score = 50
+player_scores = [0 for _ in range(players)]
+print(player_scores)
+
+while max(player_scores) < max_score:
+
+    for players_idx in range(players):
+        print(f'\nPlayer {players_idx+1} turn get started!')
+        print(f'Your total score is: {player_scores[players_idx]}')
+        current_score = 0
+
+        while True:
+            should_roll = input('Would you like to roll (y)? ').lower()
+            if should_roll != 'y':
+                break
+
+            value = roll()
+            if value == 1:
+                print('You rolled a 1! Turn done!')
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print(f'You rolled a {value}!')
+            
+            print(f'Your score is {current_score}')
+        
+        player_scores[players_idx] += current_score
+        print(f'Your total score is: {player_scores[players_idx]}')
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print(f'Player {winning_idx+1} is the winner! With a score of: {max_score}')
